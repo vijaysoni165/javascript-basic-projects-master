406b0c61c1ee8e265aba7dd98b723ed97c94df5c
vijay soni <vijay.soni@samco.in>
2025-02-17 17:40:48 +0530

    First

---
+++ 01-color-flipper/setup/app.js
@@ -0,0 +1,12 @@
+console.log(x);
+// console.log(y);
+var x = 7;
+var x = 8;
+let y = 10;
+// let y = 11;
+function getName(){
+    console.log("test");
+}
+getName();
+console.log(x);
+console.log(getName);
